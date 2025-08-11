fe173c69e2111beba4490e9e64a6c9d97837ecb0
Theneillsaaco <isaacdepena18@gmail.com>
2025-08-11 18:22:31 -0400

    try to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    site: 'https://Theneillsaaco.github.io',
+    base: '/BLLWebRoom5',
+
+    server:{
+        host: true,
+        port: 4321,
+    },
+
+    vite: {
+        plugins: [tailwindcss()]
+    },
+
+    integrations: [icon()]
 });
