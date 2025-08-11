dd0abf1504480e1e86e758402a30d34c60fc50b6
Theneillsaaco <isaacdepena18@gmail.com>
2025-08-11 17:36:27 -0400

    fist commit

---
+++ src/layouts/tailwind.config.mjs
@@ -0,0 +1,8 @@
+﻿/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}', './pages/**/*.{astro,html,js,jsx,ts,tsx}', './pages/index.astro'],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+}
