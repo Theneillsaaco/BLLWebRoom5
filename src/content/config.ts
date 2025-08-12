c130e60a9748fd31c2f7af4e5a03826120d31634
Theneillsaaco <isaacdepena18@gmail.com>
2025-08-12 09:37:09 -0400

    config Collection

---
+++ src/content/config.ts
@@ -0,0 +1,9 @@
+﻿import { defineCollection, z } from 'astro:content';
+
+const projects = defineCollection({
+    schema: z.object({
+        test: z.string()
+    })
+});
+
+export const collections = { projects };
